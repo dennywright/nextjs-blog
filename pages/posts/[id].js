16c09d653e061f9a2d357700d2dd16fa9784df75
Denny Wright <dennyw17@gmail.com>
2022-11-05 11:40:59 -0600

    Render Markdown; and some of Polishing Post Page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,19 @@
+import Head from "next/head";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
       {postData.date}
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
@@ -22,7 +27,8 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
+
   return {
     props: {
       postData,
